138823826b0ef2c6c8bc7be4c18a56d8df8ab26d
Sonam Sherpa <sherpa.sjs@gmail.com>
2023-07-14 09:13:37 -0700

    setting up backend on progress

---
+++ server/config/connection.js
@@ -0,0 +1,12 @@
+const mongoose = require("mongoose");
+
+mongoose
+  .connect(process.env.MONGODB_URI || "mongodb://localhost/alumni-books", {
+    useNewUrlParser: true,
+    useUnifiedTopology: true,
+  })
+  .then(() => {
+    console.log("db connected");
+  });
+
+module.exports = mongoose.connection;
