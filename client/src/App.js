1f168a8bc662439a65914ef69c0a62353703676c
EvelynMS1 <102782127+EvelynMS1@users.noreply.github.com>
2023-07-16 02:23:29 -0700

    grid within a sheet component for the basic profile layout

--- client/src/App.js
+++ client/src/App.js
@@ -1,5 +1,6 @@
 import React from "react";
 import { Profile } from "./components";
+// import { Dashboard } from "@material-ui/icons";
 
 function App() {
   return (
--- client/src/components/Profile.js
+++ client/src/components/Profile.js
@@ -2,16 +2,51 @@ import React from "react";
 import Avatar from "@mui/material/Avatar";
 import flowerImage from "../images/galactic-flower.png";
 import Sheet from "@mui/joy/Sheet";
+import Box from "@mui/joy/Box";
+
+import Typography from "@mui/joy/Typography";
+import { Container } from "@mui/material";
 
 //Displaying user avatar //variable for uploaded file of user
 export function Profile() {
   return (
     <Sheet>
-      Holy sheet!
-      <Avatar
-        alt="A beautiful flower of pink color within a class bubble casing"
-        src={flowerImage}
-      />
+      <grid>
+        <Container>
+          <Box
+            sx={{
+              width: 300,
+              height: 300,
+              backgroundColor: "primary.dark",
+              "&:hover": {
+                backgroundColor: "primary.main",
+                opacity: [0.9, 0.8, 0.7],
+              },
+            }}
+          >
+            Holy sheet!
+            <Avatar
+              sx={{ display: "flex", alignItems: "center" }}
+              alt="A beautiful flower of pink color within a glass bubble shapped casing"
+              src={flowerImage}
+            />
+          </Box>
+        </Container>
+
+        <Box
+          sx={{
+            width: 300,
+            height: 300,
+            backgroundColor: "primary.dark",
+            "&:hover": {
+              backgroundColor: "primary.main",
+              opacity: [0.9, 0.8, 0.7],
+            },
+          }}
+        >
+          <Typography sx={{ flex: 1 }}>Example Profile 2</Typography>
+        </Box>
+      </grid>
     </Sheet>
   );
 }
