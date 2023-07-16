c310d44a3c123354ec6353f1fb16e7cc4a359790
EvelynMS1 <102782127+EvelynMS1@users.noreply.github.com>
2023-07-15 22:44:18 -0700

    Created a sheet and wrapped contents withing the sheet as a main template

--- client/src/components/Profile.js
+++ client/src/components/Profile.js
@@ -1,12 +1,19 @@
-import React from 'react';
-import Avatar from '@mui/material/Avatar';
+import React from "react";
+import Avatar from "@mui/material/Avatar";
+import flowerImage from "../images/galactic-flower.png";
+import Sheet from "@mui/joy/Sheet";
 
-import flowerImage from '../images/galactic-flower.png';
-
-function Profile() {
-    return(
-        <Avatar alt="Galactic-flower" src={flowerImage} />
-    );
+//Displaying user avatar //variable for uploaded file of user
+export function Profile() {
+  return (
+    <Sheet>
+      Holy sheet!
+      <Avatar
+        alt="A beautiful flower of pink color within a class bubble casing"
+        src={flowerImage}
+      />
+    </Sheet>
+  );
 }
 
 export default Profile;
