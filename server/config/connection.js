5c7e39cdd5528e76224c7d48e6e38fc3c14e5998
Junnel Padilla <junnelpadilla307@gmail.com>
2023-07-25 20:17:48 -0700

    deployed link to heroku

--- server/config/connection.js
+++ server/config/connection.js
@@ -1,19 +1,13 @@
 // import mongoose module, 
-const mongoose = require("mongoose");
+// connection.js
 
-// establish a connection to the MongoDB database.
-mongoose
-  .connect(
-    process.env.MONGODB_URI || "mongodb://127.0.0.1:27017/alumni-books",
-    {
-      // set to ensure the usage of the new URL parser and the new server discovery and monitoring engine, respectively.
-      useNewUrlParser: true,
-      useUnifiedTopology: true,
-    }
-  )
-  .then(() => {
-    console.log("Connected to MongoDB");
-  })
-  .catch((err) => console.error("Failed to connect to MongoDB:", err));
-// export module
+const mongoose = require('mongoose');
+
+const dbURI = process.env.MONGODB_URI;
+mongoose.connect(dbURI, {
+  useNewUrlParser: true,
+  useUnifiedTopology: true,
+});
+
 module.exports = mongoose.connection;
+
